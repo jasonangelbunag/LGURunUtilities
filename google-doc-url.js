ccc61c91e177e0e2aa2e4ac7a54b57d03194bcf0
jasonangel <60204355+jasonangelbunag@users.noreply.github.com>
2024-01-09 09:17:13 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UoCVe62MRjyMP5GpLbTes3aZTKX4jV4EJUhoZ6T-QOU/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UoCVe62MRjyMP5GpLbTes3aZTKX4jV4EJUhoZ6T-QOU/';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
